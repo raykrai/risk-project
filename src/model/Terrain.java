2e629c1af4532d66cf875c8d33270b3f7faffcf6
José María Ruiz Laguna <josemruizlaguna@gmail.com>
2018-12-12 17:51:41 +0100

    Declaración inicial de atributos y constructores de la clase POJO 'Terrain'

--- src/model/Terrain.java
+++ src/model/Terrain.java
@@ -1,5 +1,35 @@
 package model;
 
 public class Terrain {
+	
+	//Atributos
+	
+	private String tipo;
+	
+	//Constructores
+	
+	public Terrain() {
+		
+	}
+	
+	public Terrain(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	//Getters and setters
+	
+	public String getTipo() {
+		return tipo;
+	}
 
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	//toString
+	
+	@Override
+	public String toString() {
+		return "Terrain [tipo=" + tipo + "]";
+	}	
 }
