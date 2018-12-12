388f61842818ff35ab3dc10479accf1324083edd
LucasAmado <lucasamado99@gmail.com>
2018-12-12 22:23:04 +0100

    POJO Player

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,5 +1,42 @@
 package model;
 
 public class Player {
+	
+	//Atributos
+	private String equipo;
+	
+	
+	//Constructores
+	public Player(String equipo) {
+		this.equipo = equipo;
+	}
+	
+	public Player() {
+	
+	}
+
+	//Getters y setters
+	public String getEquipo() {
+		return equipo;
+	}
+
+	public void setEquipo(String equipo) {
+		this.equipo = equipo;
+	}
+
+	
+	//toString
+	@Override
+	public String toString() {
+		return "Player [equipo=" + equipo + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
