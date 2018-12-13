ee57a2a4b7d0044d625ed1dceb9ac99dccc2b4ce
Juan Antonio Lago <juan.antonio.lago.97@gmail.com>
2018-12-13 12:36:55 +0100

    Crud terreno cambiado.JL

---
+++ src/crud/CrudTerrain.java
@@ -0,0 +1,36 @@
+package crud;
+
+import model.Terrain;
+import model.Troop;
+
+public class CrudTerrain {
+	
+	Terrain t;
+	Troop tp;
+	
+	public Terrain crearTerrenoMont() {
+		
+		t.setTipo("Montaña");
+	
+		
+		return t;
+	}
+	
+	public Terrain crearTerrenoBos() {
+		t.setTipo("Bosque");
+		return t;
+		
+	}
+	
+	public Terrain crearTerrenoLlan() {
+		t.setTipo("Llanura");
+		return t;
+		
+	}
+	
+	public Terrain crearTerrenoArt() {
+		t.setTipo("Artilleria");
+		return t;
+	}
+
+}
