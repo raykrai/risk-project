ad0d450d8cb268f19611abe033a0edb60baffb8d
Juan Antonio Lago <juan.antonio.lago.97@gmail.com>
2018-12-13 09:27:01 +0100

    Cambio tropas, Juan Antonio

--- src/model/Troop.java
+++ src/model/Troop.java
@@ -1,5 +1,50 @@
 package model;
 
 public class Troop {
+	
+	private String tipo;
+	private int cantidad;
+	
+	
+	public Troop() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Troop(String tipo, int cantidad) {
+		super();
+		this.tipo = tipo;
+		this.cantidad = cantidad;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Troop [tipo=" + tipo + ", cantidad=" + cantidad + "]";
+	}
+	
+	
+	
 
 }
