cb554b88a6d1d9a38203cdb8d59c905efa3f1aad
Lucas Amado Santos <amadolucas99@gmail.com>
2018-12-13 12:22:28 +0100

    Empezando métodos de CrudPlayer

--- src/crud/CrudPlayer.java
+++ src/crud/CrudPlayer.java
@@ -1,5 +1,28 @@
 package crud;
 
+import data.DataPlayer;
+import model.Player;
+
 public class CrudPlayer {
+	
+	//Constructor
+	public CrudPlayer() {
+		
+	}	
+	
+	
+	//Métodos
+	public Player createPlayer(String equipo) {
+		Player p=new Player(equipo);
+		
+		return p;	
+	}
+	
+	public void deletePlayer(DataPlayer arrayJugadores,int cantTropas) {
+		if(cantTropas==0) {
+			
+	}
+
+}
 
 }
