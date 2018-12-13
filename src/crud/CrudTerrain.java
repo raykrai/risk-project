a6e56695a6765e5f9a63fee2690a85826f0438ee
LucasAmado <lucasamado99@gmail.com>
2018-12-13 20:10:12 +0100

    Empezando los métodos del crudPlayer

--- src/crud/CrudTerrain.java
+++ src/crud/CrudTerrain.java
@@ -1,6 +1,5 @@
 package crud;
 
-<<<<<<< HEAD
 import model.Terrain;
 import model.Troop;
 
@@ -33,8 +32,8 @@ public class CrudTerrain {
 		t.setTipo("Artilleria");
 		return t;
 	}
-=======
-public class CrudTerrain {
->>>>>>> 7095f9ecaae49f26ae975d9cb96e9c25985f2d9d
-
+	
+	
+	
 }
+
